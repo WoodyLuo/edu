f37e29ca156fc146053d3ea7051022729d46c065
WoodyLuo <ability1013@gmail.com>
2024-06-27 15:44:16 +0800

    Adding a blocklyduino web editor.

--- serial/chjhs_web_serial.js
+++ serial/chjhs_web_serial.js
@@ -294,11 +294,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function goToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 
 
 /*
